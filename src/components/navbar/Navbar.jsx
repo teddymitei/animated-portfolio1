fd0197a690303cfa1bfa645331b6e418abd2b1b1
Maverick-ke <106772779+Maverick-ke@users.noreply.github.com>
2024-04-18 13:20:25 +0300

    Update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,18 @@ import "./navbar.scss"
 
 const Navbar = () =>{
     return (
-        <div className="navbar">Navbar</div>
+        <div className="navbar">
+            {/* Sidebar*/}
+            <div className="wrapper">
+                <span>Teddy</span>
+                <div className="social">
+                    <a href="#"><img src="/facebook.png" alt="" /></a>
+                    <a href="#"><img src="/instagram.png" alt="" /></a>
+                    <a href="#"><img src="/youtube.png" alt="" /></a>
+                    <a href="#"><img src="/dribbble.png" alt="" /></a>
+                </div>
+            </div>
+        </div>
     )
 }
 
