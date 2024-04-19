a7a069b273160ee96bfffffc92d20f1a7ec96716
Maverick-ke <106772779+Maverick-ke@users.noreply.github.com>
2024-04-19 09:56:10 +0300

    animation

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,13 +1,15 @@
-import {motion} from "framer-motion"
+import { motion } from "framer-motion"
 
 const Test = () => {
     return (
         <div className="course">
             <motion.div 
             className="box" 
-            initial={{opacity:0, scale:0.5}} 
-            animate={{opacity:1, scale:1}} 
-            transition={{duration:2, delay:2}}>
+            initial={{opacity:0.5, scale:0.5}} 
+            transition={{duration:2}}
+            whileInView={{opacity:1, scale:2}}
+            drag
+            >    
 
             </motion.div>
         </div>
